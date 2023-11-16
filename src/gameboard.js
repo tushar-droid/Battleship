6a7e39be0a010d75874922210356399ddff7f3d8
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-11-16 02:01:34 -0800

    End of day working on receive attack in gameboard todo areAllSunk next still need to write the tests for both

--- src/gameboard.js
+++ src/gameboard.js
@@ -15,18 +15,46 @@ class Gameboard{
         }
     };
 
-    placeShips = (coordinates = ["2,3","2,4", "2,5", "2,6"]) =>{
+    placeShips = () =>{
         //if needed we can also make the ship object here and then get the coordinates for its placement using obj.taken_coords;
+        const new_ship = new Ship(4,'carrier', '0,0','V');
+        const coordinates = new_ship.taken_coords;
         coordinates.forEach(coord =>{
-            this.Board.set(coord, 'S');
+            this.Board.set(coord, new_ship);
         });
     }
 
-    receiveAttack = () =>{
+    receiveAttack = (hit_coordinates_string) =>{
         //will recieve the attacks and decide if the ship is hit or not
+        // const hit_coordinates_arr = hit_coordinates_string.split(',');    //I am assuming the coordinates come as string like "2,3" or else we can use this
+        // X_coordinates = hit_coordinates_arr[0];
+        // Y_coordinates = hit_coordinates_arr[1];
+        
+        if(this.Board.get(hit_coordinates_string) instanceof Object){
+            //it is a hit
+            //call hit function in ship;
+            const hit_ship = this.Board.get(hit_coordinates_string);
+            hit_ship.hit();
+            //check if the ship is sunk
+            this.Board.set(hit_coordinates_string, 'H');    
+            //check if all ships sunk        
+            
+            this.areAllSunk();
+            
+            //start here
+
+
+        }
+        else{
+            //It hit the water so change it to a hit to make sure it is not hittable again
+            this.Board.set(hit_coordinates_string, 'H'); 
+        }
     }
 
     areAllSunk = () =>{
+
+        //todo
+
 
     }
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -2,15 +2,14 @@ const gameboard = require('./gameboard');
 
 test('check if the Ship was placed properly', () =>{
     const gm = new gameboard();
-    gm.placeShips(["2,3","2,4", "2,5", "2,6"]);
-    const expectedMap = [
-        ["2,3", 'S'],
-        ["2,4", 'S'],
-        ["2,5", 'S'],
-        ["2,6", 'S'],
-    ];
+    gm.placeShips();
+    const key_arr = ["0,1","0,2","0,3","0,0"];
     const receivedMap = gm.Board;
-    expect(Array.from(receivedMap.entries())).toEqual(expect.arrayContaining(expectedMap));
+    expect(receivedMap.get(key_arr[0])).toBeInstanceOf(Object);
+    expect(receivedMap.get(key_arr[1])).toBeInstanceOf(Object);
+    expect(receivedMap.get(key_arr[2])).toBeInstanceOf(Object);
+    expect(receivedMap.get(key_arr[3])).toBeInstanceOf(Object);
+    //expect(Array.from(receivedMap.entries())).toEqual(expect.arrayContaining(expectedMap));
 
     // expect(obj.taken_coords).toStrictEqual(["2,3","2,4", "2,5", "2,6"]);
 });
